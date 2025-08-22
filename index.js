d9b09954459cefb8046c72b1240bb30c81d95478
info-creator-nilreb <info@edelweissdesigns.de>
2025-08-22 11:51:32 +0200

    Delete index.js
    c

--- index.js
+++
@@ -1,9 +0,0 @@
-function hello(name) {
-  return `Hallo, ${name}! Willkommen bei meinem ersten Codex-Projekt.`;
-}
-
-// Testausgabe
-console.log(hello("Welt"));
-
-// Export für Tests
-module.exports = { hello };
